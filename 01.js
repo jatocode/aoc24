933f2a02adbd002da660a6ea357cb53e5104b8a2
Tobias <tobias.jansson@gmail.com>
2024-12-01 09:13:57 +0100

    reduce

--- 01.js
+++ 01.js
@@ -7,24 +7,16 @@ const lines = data.split('\n')
 let left = []
 let right = []
 lines.forEach(x => {
-    let match = x.match(/(\d+)\s+(\d+)/)
+    const match = x.match(/(\d+)\s+(\d+)/)
     left.push(parseInt(match[1]))
     right.push(parseInt(match[2]))
 })
-let lsorted = left.sort((a, b) => a - b)
-let rsorted = right.sort((a, b) => a - b)
+const lsorted = left.sort((a, b) => a - b)
+const rsorted = right.sort((a, b) => a - b)
 
-let tot = 0 
-let simscore = 0
-for (let i = 0; i < lsorted.length; i++) {
-    let l = lsorted[i]
-    let r = rsorted[i]
-    tot += Math.abs(l - r)
-
-    let lcount = right.filter(r => r === l).length
-    simscore += l * lcount
-}
+// Skrev om det till reduce bara för att se om jag kunde. Blir två loopar men coolare
+const tot = lsorted.reduce((prev, val, i) => prev + Math.abs(rsorted[i] - val), 0)
+const simscore = lsorted.reduce((prev, val, i) => prev + val * right.filter(r => r === val).length, 0)
 
 console.log('Del 1: ' + tot)
 console.log('Del 2: ' + simscore)
-
