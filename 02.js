ffaed9c08bded7493e16a71ac6f3f90530519c37
Tobias Jansson <Tobias.Jansson@prevas.se>
2024-12-02 10:17:27 +0100

    dag 2.1

--- 02.js
+++ 02.js
@@ -5,11 +5,30 @@ const data = fs.readFileSync(args[0], 'utf8')
 const lines = data.split('\n')
 
 const levels = []
-let safe = 0
+let tot = 0
 lines.forEach(line => {
     const level = line.split(' ').map(x => parseInt(x))
-    let safe = false
-    for(let i=1;i<level.l;i++) {
-        if(level[i] )
+    levels.push(level)
+    let inc = 0
+    let dec = 0
+    let safe = true
+    level.forEach((x, i) => {
+        const next = level[i+1]
+        if(next != undefined && x-next > 0) inc ++
+        if(next != undefined && x-next < 0) dec ++
+        if(!checkSafe(x, next)) safe = false
+    })
+
+    if(safe && (inc == level.length - 1 || dec == level.length - 1)) {
+        tot += 1
     }
 })
+
+console.log('Del 1:', tot)
+    
+function checkSafe(a,b) {
+    if(b != undefined) {
+        return Math.abs(a-b) > 0 && Math.abs(a-b) < 4
+    }
+    return true
+}
