7220c154c9565afaea697bab229e727acc75d301
Tobias Jansson <Tobias.Jansson@prevas.se>
2024-12-03 11:14:31 +0100

    3.2

--- 03.js
+++ 03.js
@@ -10,20 +10,45 @@ let line = ''
 lines.forEach(l => {
     line += l
 });
-const match = line.matchAll(/(mul\((\d+),(\d+)\))/g)
-const conds = line.matchAll(/(don\'t\(\).*?do\(\))/g)
-const disabledParts = []
-for (const c of conds) {
-    disabledParts.push([c.index, c.index + c[1].length])
-}
-for (const m of match) {
-    const disabled = disabledParts.filter(d => {
-    //    console.log(m[0], m.index, d[0], d[1], m.index > d[0] && m.index < d[1])
-        return d[1], m.index > d[0] && m.index < d[1]
-    }).length > 0
-    sum += parseInt(m[2]) * parseInt(m[3])
-    if (!disabled) sum2 += parseInt(m[2]) * parseInt(m[3])
-}
+
+sum = sumMul(line)
+sum2 = sumMulWithCond(line)
 
 console.log('Del 1:', sum)
 console.log('Del 2:', sum2)
+
+function sumMul(line) {
+    const match = line.matchAll(/(mul\((\d+),(\d+)\))/g)
+    let sum = 0
+    for (const m of match) {
+        sum += parseInt(m[2]) * parseInt(m[3])
+    }
+    return sum
+}
+
+function sumMulWithCond(line) {
+    const match = line.matchAll(/(mul\((\d+),(\d+)\)|do\(\)|don\'t\(\))/g)
+    let sum = 0
+    let enabled = true
+    for (const m of match) {
+        if (m[1] == undefined) continue
+        if (m[1] == 'do()') enabled = true
+        if (m[1] == 'don\'t()') enabled = false
+
+        if (enabled && m[1].startsWith('mul')) sum += parseInt(m[2]) * parseInt(m[3])
+    }
+    return sum
+}
+
+// Fick den inte att funka :D :'( 
+function sumMulWithCondNotworking(line) {
+    const match = line.matchAll(/(mul\((\d+),(\d+)\))/g)
+    let sum = 0
+    for (const m of match) {
+        const dos = 1 + line.slice(0, m.index).matchAll(/(do\(\))/g).toArray().length // vi börjar med ett do, så lägg på 1
+        const donts = line.slice(0, m.index).matchAll(/(don\'t\(\))/g).toArray().length
+        if (donts > dos) continue
+        sum += parseInt(m[2]) * parseInt(m[3])
+    }
+    return sum
+}
