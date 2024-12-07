ec62b2712cd8fad0a4fe695a34e3e0c1d6a56c61
Tobias <tobias.jansson@gmail.com>
2024-12-07 12:59:54 +0100

    7.2, kom på en optimering

--- 07.js
+++ 07.js
@@ -7,11 +7,13 @@ const lines = data.split('\n')
 
 let sum = 0
 let sum2 = 0
+let calibs = []
 lines.forEach((line, y) => {
     if (line.length == 0) return
     const match = line.match(/(\d+):(.*)/)
     const test = parseInt(match[1])
     const values = match[2].trim().split(' ').map(x => parseInt(x))
+    calibs.push({test, values})
     sum += check(test, values) ? test : 0
     sum2 += check(test, values, true) ? test : 0
 })
@@ -20,6 +22,7 @@ console.log('Del 1', sum)
 console.log('Del 2', sum2)
 
 function check(test, values, concat = false) {
+    if (values[0] > test) return false
     if (values.length === 1) return values[0] === test 
     const rem = values.slice(2)
     const valp = [values[0] + values[1], ...rem]
