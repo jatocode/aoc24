9a64c40fd581c94a15b812a8182efc3b85c6ac3d
Tobias Jansson <tobias.jansson@prevas.se>
2024-12-12 15:14:26 +0100

    12.1 usch

--- 12.js
+++ 12.js
@@ -14,16 +14,16 @@ console.log('Del 1:', regions.map(region => regionFacts(region)).reduce((a, b) =
 
 function findAllRegions() {
     let regions = []
-    for (let y = 0; y < map.length - 1; y++) {
+    for (let y = 0; y < map.length; y++) {
         for (let x = 0; x < map[0].length; x++) {
             let found = false
             regions.forEach(region => {
-                if(region.has([x, y].join(','))) {
+                if (region.has(`${x},${y}`)) {
                     found = true
                     return
                 }
             })
-            if(!found) regions.push(findRegion([x, y]))
+            if (!found) regions.push(findRegion([x, y]))
         }
     }
     return regions
@@ -36,12 +36,11 @@ function regionFacts(region) {
     const perimeter = positions.reduce((acc, pos) => {
         const nb = neighbours(pos, type)
 
-        // Här kommer jag inte åt de som är utanför kartan
         const border = nb.filter(n => !region.has(n.join(',')))
 
-        const d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         let sumOutside = 0
-        const outside = d.forEach(dir => {
+        const d = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        d.forEach(dir => {
             const x = pos[0] + dir[0]
             const y = pos[1] + dir[1]
             if (map[y] == undefined || map[y][x] == undefined) sumOutside++
@@ -50,7 +49,7 @@ function regionFacts(region) {
         return acc + border.length + sumOutside
     }, 0)
 
-    console.log(type, 'Price:', area * perimeter, 'Area:', area, 'Perimeter:', perimeter,  )
+    //console.log(type, 'Price:', area * perimeter, 'Area:', area, 'Perimeter:', perimeter,)
     return area * perimeter
 }
 
@@ -58,18 +57,22 @@ function findRegion(start) {
     const queue = [start]
     const found = new Set()
     const type = map[start[1]][start[0]]
+    const visited = new Set()
     found.add(start.join(','))
     while (queue.length > 0) {
         const current = queue.shift()
-        const nexts = neighbours(current, type)
-        for (const next of nexts) {
-            if (found.has(next.join(','))) continue
-            queue.push(next)
+        for (const nextpos of neighbours(current, type)) {
+            const next = nextpos.join(',')
             found.add(current.join(','))
+            if(!visited.has(next)) queue.push(nextpos)
+            visited.add(next)
+            if (found.has(next)) continue
         }
     }
+    //console.log('Found region:', type, found.size)
     return found
 }
+
 
 function neighbours(pos, type) {
     const nb = [[0, 1], [0, -1], [1, 0], [-1, 0]]
